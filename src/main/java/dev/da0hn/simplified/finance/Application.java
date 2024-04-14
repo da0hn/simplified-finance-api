e1da1bdbcd062165ba3e55cfdd496bb4f3b939e1
Da0hn <gabrielhondacba@gmail.com>
2024-04-14 11:59:47 -0400

    chore: initial commit

---
+++ src/main/java/dev/da0hn/simplified/finance/Application.java
@@ -0,0 +1,13 @@
+package dev.da0hn.simplified.finance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedFinanceApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimplifiedFinanceApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dev/da0hn/simplified/finance/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.da0hn.simplified.finance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplifiedFinanceApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
