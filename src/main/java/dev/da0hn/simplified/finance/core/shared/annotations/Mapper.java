35b6e7ed5c0beaed22573343a7b456444b2167b3
Gabriel Honda <gabrielhondacba@gmail.com>
2024-05-02 21:54:39 -0400

    fix: adiciona anotação `@Component` nas anotações customizadas

--- src/main/java/dev/da0hn/simplified/finance/core/shared/annotations/Mapper.java
+++ src/main/java/dev/da0hn/simplified/finance/core/shared/annotations/Mapper.java
@@ -1,5 +1,6 @@
 package dev.da0hn.simplified.finance.core.shared.annotations;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Indexed;
 
 import java.lang.annotation.Documented;
@@ -9,6 +10,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Indexed
+@Component
 @Documented
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
