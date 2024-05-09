6038e9687ce110f64d73736c3721b0b9941b6687
Gabriel Honda <gabrielhondacba@gmail.com>
2024-05-09 12:19:15 -0400

    feat: adiciona implementação das operações de repositório de categoria `createInBatch` e `findAllById`

--- src/main/java/dev/da0hn/simplified/finance/core/ports/spi/repositories/CategoryRepository.java
+++ src/main/java/dev/da0hn/simplified/finance/core/ports/spi/repositories/CategoryRepository.java
@@ -1,18 +1,25 @@
 package dev.da0hn.simplified.finance.core.ports.spi.repositories;
 
 import dev.da0hn.simplified.finance.core.domain.Category;
+import dev.da0hn.simplified.finance.core.domain.valueobjects.CategoryId;
 import lombok.Builder;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface CategoryRepository {
 
   void create(Category category);
 
+  void createInBatch(Collection<Category> categories);
+
   Optional<Category> findByName(String name);
 
   List<Category> searchBy(CategoryFilterCriteria criteria);
+
+  Set<Category> findAllById(Collection<? extends CategoryId> categoriesId);
 
   @Builder
   record CategoryFilterCriteria(
--- src/main/java/dev/da0hn/simplified/finance/infrastructure/db/repositories/adapters/CategoryRepositoryAdapter.java
+++ src/main/java/dev/da0hn/simplified/finance/infrastructure/db/repositories/adapters/CategoryRepositoryAdapter.java
@@ -2,16 +2,22 @@ package dev.da0hn.simplified.finance.infrastructure.db.repositories.adapters;
 
 import dev.da0hn.simplified.finance.core.domain.Category;
 import dev.da0hn.simplified.finance.core.domain.validation.Validations;
+import dev.da0hn.simplified.finance.core.domain.valueobjects.CategoryId;
 import dev.da0hn.simplified.finance.core.ports.spi.repositories.CategoryRepository;
 import dev.da0hn.simplified.finance.core.shared.annotations.Adapter;
 import dev.da0hn.simplified.finance.infrastructure.db.entities.CategoryEntity;
 import dev.da0hn.simplified.finance.infrastructure.db.mappers.RepositoryDataMapper;
 import dev.da0hn.simplified.finance.infrastructure.db.repositories.CategoryJpaRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Adapter
 @AllArgsConstructor
 public class CategoryRepositoryAdapter implements CategoryRepository {
@@ -22,19 +28,35 @@ public class CategoryRepositoryAdapter implements CategoryRepository {
 
   @Override
   public void create(final Category category) {
+    log.info("method=create(category={})", category);
+
     final var categoryEntity = this.repositoryDataMapper.toEntity(category, CategoryEntity.class);
     Validations.requireNonNull(categoryEntity, "categoryEntity");
     this.categoryJpaRepository.save(categoryEntity);
   }
 
   @Override
+  public void createInBatch(final Collection<Category> categories) {
+    log.info("method=createInBatch(categories={})", categories);
+
+    final var categoryEntities = categories.stream()
+      .map(category -> this.repositoryDataMapper.toEntity(category, CategoryEntity.class))
+      .toList();
+
+    this.categoryJpaRepository.saveAll(categoryEntities);
+  }
+
+  @Override
   public Optional<Category> findByName(final String name) {
+    log.info("method=findByName(name={})", name);
     return this.categoryJpaRepository.findByName(name)
       .map(category -> this.repositoryDataMapper.toDomain(category, Category.class));
   }
 
   @Override
   public List<Category> searchBy(final CategoryFilterCriteria criteria) {
+    log.info("method=searchBy(criteria={})", criteria);
+
     final var categories = this.categoryJpaRepository.findAllBy(
       criteria.queryId(),
       criteria.queryName(),
@@ -44,6 +66,17 @@ public class CategoryRepositoryAdapter implements CategoryRepository {
     return categories.stream()
       .map(category -> this.repositoryDataMapper.toDomain(category, Category.class))
       .toList();
+  }
+
+  @Override
+  public Set<Category> findAllById(final Collection<? extends CategoryId> categoriesId) {
+    log.info("method=findAllById(categoriesId={})", categoriesId);
+
+    final var ids = categoriesId.stream().map(CategoryId::value).toList();
+
+    return this.categoryJpaRepository.findAllById(ids).stream()
+      .map(category -> this.repositoryDataMapper.toDomain(category, Category.class))
+      .collect(Collectors.toSet());
   }
 
 }
